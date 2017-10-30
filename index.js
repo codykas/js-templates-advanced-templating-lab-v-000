7cf77ecaa18932523c50bc74ba3f3f8000dea000
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 17:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function init(){
-  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-partial").innerHTML)
-  Handlebars.registerHelper("displayIngredient", function(ingredient){
+  handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML)
+  handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-partial").innerHTML)
+  handlebars.registerHelper("displayIngredient", function(ingredient){
     return new handlebars.safeString("<li name='ingredientsList'>" + ingredient + "</li>")
   })
 
