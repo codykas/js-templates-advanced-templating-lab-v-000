13b75d500172cafaee4a4dc6d0a926b86504b3e1
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 18:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function init(){
-  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-partial").innerHTML)
-  Handlebars.registerHelper("displayIngredient", function(ingredient){
-    return new Handlebars.safeString("<li name='ingredientsList'>" + ingredient + "</li>")
+  handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML)
+  handlebars.registerPartial("recipeFormPartial", document.getElementById("recipe-form-partial").innerHTML)
+  handlebars.registerHelper("displayIngredient", function(ingredient){
+    return new handlebars.safeString("<li name='ingredientsList'>" + ingredient + "</li>")
   })
 
-  let form = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+  let form = handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
 
   document.getElementsByTagName('main')[0].innerHTML = form({'submitAction': 'createRecipe()'})
 }
@@ -32,7 +32,7 @@ function createRecipe(){
     'ingredients': ingredients
   }
 
-  let template = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
+  let template = handlebars.compile(document.getElementById('recipe-template').innerHTML)
 
   document.getElementById('main').innerHTML = template(recipe)
 }
@@ -54,7 +54,7 @@ function displayEditForm(){
     'submitAction': 'updateRecipe()'
   }
 
-  let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  let template = handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
   document.getElementById("main").innerHTML = template(recipe)
 }
 
